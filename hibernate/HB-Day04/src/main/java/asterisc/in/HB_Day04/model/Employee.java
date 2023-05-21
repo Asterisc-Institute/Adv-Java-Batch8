0889c66a85578c044feb7194876222d596df76d7
chandrakant sir <cakant70@gmail.com>
2023-05-21 16:12:10 +0530

    Hibernate Day 03 and  04

---
+++ hibernate/HB-Day04/src/main/java/asterisc/in/HB_Day04/model/Employee.java
@@ -0,0 +1,53 @@
+package asterisc.in.HB_Day04.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="emp77")
+public class Employee {
+	
+	@Id
+	@Column(name="eid")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;  
+	
+	@Column
+	private String ename;
+	private String address;
+	
+	
+	public Employee() {
+		super();
+	}
+	public Employee(int id, String ename, String address) {
+		super();
+		this.id = id;
+		this.ename = ename;
+		this.address = address;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
